bec7da9e2af3e26b53db8c4576cd14af070cd568
Jake <jakelovitz@gmail.com>
2019-06-02 09:39:42 -0400

    finished Looping Code-along

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(array, event) {
+    let thankYouArray = []
+    for (let i = 0; i < array.length; i++){
+       thankYouArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouArray;
+}
+
+function countdown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--
+    }
+}
