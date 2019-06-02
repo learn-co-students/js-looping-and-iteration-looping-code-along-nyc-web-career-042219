a2ccae75ae237308d2b19efd0b67deeea830c18f
Christian Duncan <christianeduncan3@gmail.com>
2019-06-02 19:57:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array_o_names){
+    let cards = [];
+    for (let i = 0;i < array_o_names.length;i++){
+        cards[i] = "Thank you, " + array_o_names[i] + ", for the wonderful surprise gift!";
+    }
+    return cards;
+}
+
+function countdown(num){
+    while(num>=0){
+        console.log(num--);
+    }
+}
+
