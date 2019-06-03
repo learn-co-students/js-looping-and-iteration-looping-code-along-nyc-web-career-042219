dfb90edda8851bd1f7f7ccac19dacc4a9cbfe5dc
Jason Gomez <jason.gomez@me.com>
2019-06-02 23:07:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names) {
+    const array = [];
+    for (let index = 0; index < names.length; index++) {
+        array.push(`Thank you, ${names[index]}, for the wonderful surprise gift!`)
+        
+    }
+    return array;
+}
+
+function countdown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
