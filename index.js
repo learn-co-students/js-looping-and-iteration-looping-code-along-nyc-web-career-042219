8ad1bc85239e77ba916291a57c2fc00c2030daa2
Adam Sultanov <adam.sultanov@gmail.com>
2019-06-02 21:13:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+  let thanks = []
+  for (let i = 0; i < names.length; i++) {
+    thanks.push("Thank you, ${names[i]}, for the wonderful ${event} gift.");
+  }
+  return thanks;
+}
