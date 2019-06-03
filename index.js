559d74ecfd38d886b7829ed6a4e412f60dce085c
Adam Sultanov <adam.sultanov@gmail.com>
2019-06-02 21:17:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 // Code your solutions in this file
 
-function writeCards(names, event) {
+function writeCards (names, event) {
   let thanks = []
   for (let i = 0; i < names.length; i++) {
-    thanks.push("Thank you, ${names[i]}, for the wonderful ${event} gift.");
+    thanks.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
   }
   return thanks;
+}
+
+function countdown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
 }
