cc42c65eae900724ec838c2bd20b0ac7c1261aa8
gavinoconnor <gedmund@gmail.com>
2019-06-02 20:31:55 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+let eventName = 'surprise';
+
+function writeCards(names) {
+  let thanksArray = [];
+  for (let i = 0; i < names.length; i++) {
+    thanksArray.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+  }
+  return thanksArray;
+}
+
+function countdown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
