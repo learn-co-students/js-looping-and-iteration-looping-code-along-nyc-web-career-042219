12319a232ec054c13037ab6448f226aa32f44396
Won Kim <wonsae.kim@gmail.com>
2019-06-03 02:01:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(arr) {
+  const result = [];
+  for(let i = 0; i < arr.length; i++) {
+    result.push(`Thank you, ${arr[i]}, for the wonderful surprise gift!`)
+  }
+  return result;
+}
+
+function countdown(num) {
+  for(let i = num; i > -1; i--) {
+    console.log(i)
+  }
+}
