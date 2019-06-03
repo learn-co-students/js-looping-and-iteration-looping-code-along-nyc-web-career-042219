c453f7a823445cf98a8f0386b2478070d17ce486
cbatsoula <cbatsoula@gmail.com>
2019-06-03 13:24:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+  let thankYouCards = []
+  for (let i = 0; i < names.length; i++) {
+    thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return thankYouCards
+}
+function countdown(num) {
+  // let i = 10
+  while (num > 0) {
+    console.log(num);
+    num --;
+  }
+  console.log(num);
+}
