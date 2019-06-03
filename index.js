91324b498fce415130ec41fcd57d12739e8d9012
Augustus Kirby <kirbyaugustus@gmail.com>
2019-06-03 17:53:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+
+function writeCards(arr, event){
+  let cards = []
+  for(let i = 0; i < gifts.length; i++){
+   cards.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`)
+  }
+  return cards
+}
+  
+  
+  
+  function countdown(num){
+    while (0 < num){
+      console.log(num)
+      num--
+    }
+    console.log(num)
+  }
