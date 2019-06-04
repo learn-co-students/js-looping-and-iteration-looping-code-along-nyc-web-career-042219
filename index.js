e6c59f86fb4c8afe41f85f12c150e2fe8c88caa3
Qunli <qunli02@gmail.com>
2019-06-04 17:22:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(array, eve) {
+  let answer = []
+  for (let i = 0; i < array.length; i++) {
+    var sentence = (`Thank you, ${array[i]}, for the wonderful surprise gift!`);
+    answer.push(sentence)
+  }
+
+  return answer;
+}
+
+function countdown(num){
+  let count = num;
+  while (count >= 0) {
+    console.log(count--);
+  }
+}
