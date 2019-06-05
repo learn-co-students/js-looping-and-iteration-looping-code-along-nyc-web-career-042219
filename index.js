4a8ddbeffd87c50c606f5e5ee590bbbec0f9ce6c
jedspears <jed.spears7@gmail.com>
2019-06-05 13:38:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function countdown(num) {
+  while(num >= 0) {
+    console.log(num--);
+  }
+}
+
+function writeCards(names) {
+  let gaga = []
+  for(let i = 0, k = names.length; i < k; i++) {
+    gaga.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+  }
+  return gaga;
+}
